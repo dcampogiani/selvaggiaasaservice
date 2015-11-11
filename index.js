e899c5b66854905826826fdaa651c8a479207cc6
Daniele Campogiani <daniele.campogiani@gmail.com>
2015-11-11 21:11:41 +0100

    Initial commit

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/selvaggiona', function(request, response) {
+  response.send('selvaggiona');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
