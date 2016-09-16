97b3f95d8f1f996b3286a5cedef525bbb7b00595
Daniele Campogiani <daniele.campogiani@gmail.com>
2016-09-16 22:34:14 +0200

    Since google-images is not working anymore without auth, use predefined data

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var client = require('google-images');
 var requestLib = require('request');
 
 function randomIntInc(low, high) {
@@ -21,17 +20,25 @@ app.get('/', function(request, response) {
 
 app.get('/selvaggiona', function(request, response) {
 
-  client.search('Selvaggia Lucarelli', {
-    page: randomIntInc(1, 10),
-    callback: function(err, images) {
-      if (err)
-        console.error(err);
-      else {
-        var image = images[randomIntInc(0, images.length - 1)];
-        requestLib.get(image.url).pipe(response);
-      }
-    }
-  });
+  var data = [
+    "http://www.siciliainformazioni.com/wp-content/uploads/2016/08/Selvaggia-Lucarelli-1.jpg",
+    "http://media.gossipblog.it/9/9df/selvaggia-lucarelli-foto-sky.jpg",
+    "http://www.chedonna.it/wp-content/uploads/Selvaggia_Lucarelli.jpg",
+    "http://www.mywhere.it/wp-content/uploads//2014/06/selvaggia-lucarelli.jpg",
+    "http://gds.it.cdn-immedia.net/2015/01/Selvaggia-Lucarelli-9.jpg",
+    "http://ilarge.lisimg.com/image/2826136/1037full-selvaggia-lucarelli.jpg",
+    "http://www.speakerscorner.me/wp-content/uploads/2016/04/selvaggia7.jpg",
+    "https://s-media-cache-ak0.pinimg.com/736x/9e/d7/26/9ed726487342d1bc5e3923dc5691ff34.jpg",
+    "http://www.bellaweb.it/files/2013/03/Selvaggia-Lucarelli.jpg",
+    "http://www.lettera43.it/upload/images/11_2014/l43-selvaggia-lucarelli-141118170545_big.jpg",
+    "http://www.superstarz.com/wp-content/uploads/2015/01/SELVAGGIA-LUCARELLI-NAPOLI.jpg"
+  ];
+
+  var index = randomIntInc(0,data.length - 1);
+  var imageUrl = data[index];
+
+  requestLib.get(imageUrl).pipe(response);
+
 });
 
 app.listen(app.get('port'), function() {
